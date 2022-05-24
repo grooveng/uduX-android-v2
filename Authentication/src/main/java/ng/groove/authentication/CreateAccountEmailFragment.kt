7401baebf54af6bf98bbd5fa214e7a55feaa7dd9
Taofeek’s MacBook Pro <oduola.taofeekkola@gmail.com>
2022-05-24 15:59:41 +0100

    UI implementation for sign up authentication

--- Authentication/src/main/java/ng/groove/authentication/CreateAccountEmailFragment.kt
+++ Authentication/src/main/java/ng/groove/authentication/CreateAccountEmailFragment.kt
@@ -6,14 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import ng.groove.authentication.databinding.FragmentFirstBinding
+import ng.groove.authentication.databinding.FragmentCreateAccountEmailBinding
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class CreateAccountEmailFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentCreateAccountEmailBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +25,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentCreateAccountEmailBinding.inflate(inflater, container, false)
         return binding.root
 
     }
